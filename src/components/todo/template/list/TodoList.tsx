838188e347359b7ee2b609183999a884f0d1a4d6
salybu <rkdmsdl93@gmail.com>
2021-08-21 15:57:04 +0900

    [FEAT] 정렬추가

---
+++ src/components/todo/template/list/TodoList.tsx
@@ -0,0 +1,36 @@
+import { Itodo } from 'components/todo/TodoService';
+import React from 'react';
+import styled from 'styled-components';
+import TodoItem from './item/TodoItem';
+
+const TodoListBlock = styled.div`
+  flex: 1;
+  padding: 20px 32px;
+  padding-bottom: 48px;
+  overflow-y: auto;
+`;
+
+interface TodoListProps {
+  todos: Itodo[];
+  toggleTodo: (id: number) => void;
+  removeTodo: (id: number) => void;
+}
+
+const TodoList = ({ toggleTodo, removeTodo, todos }: TodoListProps) => {
+  return (
+    <TodoListBlock>
+      {todos &&
+        todos
+          .sort((a, b) => {
+            const dateA = new Date(a.targetDate).getTime();
+            const dateB = new Date(b.targetDate).getTime();
+            return dateA - dateB;
+          })
+          .filter((x) => x.done === false)
+          .concat(todos.filter((x) => x.done === true))
+          .map((todo) => <TodoItem toggleTodo={toggleTodo} removeTodo={removeTodo} key={todo.id} todo={todo} />)}
+    </TodoListBlock>
+  );
+};
+
+export default React.memo(TodoList);
